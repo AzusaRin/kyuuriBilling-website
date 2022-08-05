136ff5ec0c921b1e54b29f99830f0f3d4679f74b
Azusa <qq745847188@gmail.com>
2022-08-05 10:50:18 +0800

    deploy

---
+++ precache-manifest.be21dec6a583a3cd0c35db2b38b4613e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a723dc3e47e18560bb39",
+    "url": "/kyuuriBilling-website/css/app.f65bd62f.css"
+  },
+  {
+    "revision": "90b1b432f0065bcd2953",
+    "url": "/kyuuriBilling-website/css/chunk-vendors.058a81fe.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/kyuuriBilling-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/kyuuriBilling-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ed556af7b360f5500a1b8566c7e51b23",
+    "url": "/kyuuriBilling-website/index.html"
+  },
+  {
+    "revision": "a723dc3e47e18560bb39",
+    "url": "/kyuuriBilling-website/js/app.b696f4b5.js"
+  },
+  {
+    "revision": "90b1b432f0065bcd2953",
+    "url": "/kyuuriBilling-website/js/chunk-vendors.b36e9717.js"
+  },
+  {
+    "revision": "497041096cc17899996dd70142ebec4a",
+    "url": "/kyuuriBilling-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kyuuriBilling-website/robots.txt"
+  }
+]);
